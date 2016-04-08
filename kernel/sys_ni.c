8e3ab8956def2c1da9bdbe3954d67c839177d1ca
PaoloW8 <paolom970@gmail.com>
2016-04-08 12:29:35 +0200

    update to rel V7

--- kernel/sys_ni.c
+++ kernel/sys_ni.c
@@ -25,6 +25,7 @@ cond_syscall(sys_swapoff);
 cond_syscall(sys_kexec_load);
 cond_syscall(compat_sys_kexec_load);
 cond_syscall(sys_init_module);
+cond_syscall(sys_finit_module);
 cond_syscall(sys_delete_module);
 cond_syscall(sys_socketpair);
 cond_syscall(sys_bind);
@@ -203,3 +204,7 @@ cond_syscall(sys_fanotify_mark);
 cond_syscall(sys_name_to_handle_at);
 cond_syscall(sys_open_by_handle_at);
 cond_syscall(compat_sys_open_by_handle_at);
+
+/* operate on Secure Computing state */
+cond_syscall(sys_seccomp);
+
